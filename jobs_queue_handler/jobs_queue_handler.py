7e28f066119c916535e6f598637540a915c8540f
Simon Lacasse <simon.lacasse@bnc.ca>
2020-12-03 21:58:11 -0500

    adding api key for simple protection

--- jobs_queue_handler/jobs_queue_handler.py
+++ jobs_queue_handler/jobs_queue_handler.py
@@ -1,6 +1,22 @@
 from flask import Flask
+from flask import request
+from flask import abort
+from os import getenv
+
+API_KEY = getenv('API_KEY')
+
 app = Flask(__name__)
+
+@app.before_request
+def validate_api_key():
+    if request.headers['API_KEY'] != API_KEY:
+        abort(403, description="Missing API key")
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+# @app.route('/jobs', methods=['POST'])
+# def hello_world():
+#     return 'Hello, World!'
+
