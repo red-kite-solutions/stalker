67e878ab8496713e910e9e41d23d60a09056065c
Simon Lacasse <simon.lacasse@bnc.ca>
2020-12-03 23:08:44 -0500

    adding json object content validation for a route for queuing a new job

--- jobs_queue_handler/jobs_queue_handler.py
+++ jobs_queue_handler/jobs_queue_handler.py
@@ -1,7 +1,6 @@
-from flask import Flask
-from flask import request
-from flask import abort
+from flask import Flask, request, abort
 from os import getenv
+import json
 
 API_KEY = getenv('API_KEY')
 
@@ -16,7 +15,16 @@ def validate_api_key():
 def hello_world():
     return 'Hello, World!'
 
-# @app.route('/jobs', methods=['POST'])
-# def hello_world():
-#     return 'Hello, World!'
+@app.route('/jobs', methods=['POST'])
+def add_job_to_queue():
+    json_request_data = json.loads(request.data) 
+
+    if (not json_request_data.get("id") or not json_request_data.get("task") or
+            json_request_data.get("priority") == None or 
+            not json_request_data.get("data")):
+        print(json_request_data)
+        abort(400, description="The json object did not contain all the required fields.")
+    
+        
+    return 'Hello, World!'
 
