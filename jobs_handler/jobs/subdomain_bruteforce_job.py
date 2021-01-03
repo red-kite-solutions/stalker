f240da5cb6afaea5cfcefe5c336942911481fbfa
Simon Lacasse <simon.lacasse@bnc.ca>
2021-01-03 10:52:27 -0500

    added an environement config to remove commented code and put an if instead for testing purposes

--- jobs_handler/jobs/subdomain_bruteforce_job.py
+++ jobs_handler/jobs/subdomain_bruteforce_job.py
@@ -10,6 +10,7 @@ class SubdomainBruteforceJob(JobInterface):
     _wordlist: str
     _config_file: str
     _amass_bin_path: str
+    _env: str
 
     def __init__(self, job_info: dict, config: dict):
         super().__init__(job_info['_id'], job_info['_task'])
@@ -25,31 +26,31 @@ class SubdomainBruteforceJob(JobInterface):
         self._wordlist = wordlist
         self._config_file = config['amass_config']
         self._amass_bin_path = config['amass_bin_path']
+        self._env = config['env']
 
     def run(self):
         """Start the job"""
-        print("brute force job is running")
-        print(self._domain_name)
-        print(self._wordlist)
-        print(self._config_file)
-        print(self._amass_bin_path)
-        return
-        # amass_string = ''
-        # if self._domain_name != '':
-        #     amass_string += f'{self._amass_bin_path}amass enum -brute -d {self._domain_name}'
-        # else:
-        #     raise Exception('Missing domain name for subdomain bruteforcing.')
+        if self._env == 'DEV':
+            print("brute force job is running")
+            print(self._domain_name)
+            print(self._wordlist)
+            print(self._config_file)
+            print(self._amass_bin_path)
+        else:
+            amass_string = ''
+            if self._domain_name != '':
+                amass_string += f'{self._amass_bin_path}amass enum -brute -d {self._domain_name}'
+            else:
+                raise Exception('Missing domain name for subdomain bruteforcing.')
 
-        # if self._wordlist != '':
-        #     amass_string += f' -w {self._wordlist}'
-        
-        # if self._config_file != '':
-        #     amass_string += f' -config {self._config_file}'
+            if self._wordlist != '':
+                amass_string += f' -w {self._wordlist}'
+            
+            if self._config_file != '':
+                amass_string += f' -config {self._config_file}'
 
-        # print(amass_string)
-        # stream = os.popen(amass_string)
-        # output = stream.readlines()
-        # fixed_output = [x[:-1] for x in output] # remove trailing \n
-        # print(fixed_output)
+            stream = os.popen(amass_string)
+            output = stream.readlines()
+            fixed_output = [x[:-1] for x in output] # remove trailing \n
 
 
