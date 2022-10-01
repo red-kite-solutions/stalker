90a4f1874f1dda3d139bdd7eb5083471918f4da4
Antoine Boisier-Michaud <antoine.boisier-michaud@cactusoft.ca>
2022-10-01 00:10:50 -0400

    Remove redundant test

--- orchestrator/Orchestrator.Tests/JobSerializerTests.cs
+++ orchestrator/Orchestrator.Tests/JobSerializerTests.cs
@@ -1,7 +1,6 @@
 using Confluent.Kafka;
 using FluentAssertions;
 using Orchestrator.Queue.JobsConsumer;
-using System.Text;
 
 namespace Orchestrator.Tests;
 
@@ -20,16 +19,6 @@ public class JobSerializerTests
 
         // Act & Assert
         AssertRoundTrip(job);
-    }
-
-    [Fact]
-    public void Deserialize_String_Workd()
-    {
-        // Arrange
-        var serialized = "{\"jobId\":\"6337aced7193bb0125cdb4ae\",\"task\":\"DomainNameResolvingJob\",\"domainName\":\"amazon.com\",\"companyId\":\"63378fcfa4c49800434582c3\"}";
-
-        var deserializer = new JobSerializer<JobRequest>();
-        var actual = deserializer.Deserialize(Encoding.ASCII.GetBytes(serialized), false, SerializationContext.Empty);
     }
 
     private void AssertRoundTrip<T>(T expected) where T : JobRequest
