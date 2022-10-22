2e291119fd32f171253b496acd072ac7f6f89807
Antoine Boisier-Michaud <antoine.boisier-michaud@cactusoft.ca>
2022-10-22 11:37:53 -0400

    Validate the job and the company before executing findings handler

--- flow_manager/src/modules/findings/commands/hostname-ip.handler.ts
+++ flow_manager/src/modules/findings/commands/hostname-ip.handler.ts
@@ -1,23 +1,30 @@
+import { Logger } from '@nestjs/common';
 import { CommandHandler } from '@nestjs/cqrs';
 import { JobsService } from 'src/modules/database/jobs/jobs.service';
 import { Job } from 'src/modules/database/jobs/models/jobs.model';
 import { CompanyService } from 'src/modules/database/reporting/company.service';
+import { Company } from '../../database/reporting/company.model';
 import { HostService } from '../../database/reporting/host/host.service';
 import { FindingHandlerBase } from './findings-handler-base';
 import { HostnameIpCommand } from './hostname-ip.command';
 
 @CommandHandler(HostnameIpCommand)
 export class HostnameIpHandler extends FindingHandlerBase<HostnameIpCommand> {
+  protected logger: Logger = new Logger('HostnameIpHandler');
+
   constructor(
-    jobService: JobsService,
-    private companyService: CompanyService,
     private hostService: HostService,
+    jobService: JobsService,
+    companyService: CompanyService,
   ) {
-    super(jobService);
+    super(jobService, companyService);
   }
 
-  protected async executeCore(job: Job, command: HostnameIpCommand) {
-    const company = await this.companyService.get(job.companyId);
+  protected async executeCore(
+    job: Job,
+    company: Company,
+    command: HostnameIpCommand,
+  ) {
     await this.hostService.addHostsWithDomain(
       command.ips,
       command.domainName,
