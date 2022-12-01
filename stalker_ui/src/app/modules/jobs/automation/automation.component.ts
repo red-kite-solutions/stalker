db5c98cb64262b4b6e6bda62626b181a51b61c90
lm-sec <sim.lacasse1239@gmail.com>
2022-12-01 00:07:44 -0500

    adding the monaco text editor to a flexible component that should answer most of our needs

--- stalker_ui/src/app/modules/jobs/automation/automation.component.ts
+++ stalker_ui/src/app/modules/jobs/automation/automation.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CodeEditorService } from 'src/app/shared/widget/code-editor/code-editor.service';
 
 @Component({
   selector: 'app-automation',
   templateUrl: './automation.component.html',
-  styleUrls: ['./automation.component.scss']
+  styleUrls: ['./automation.component.scss'],
 })
-export class AutomationComponent implements OnInit {
+export class AutomationComponent {
+  public code = 'asdf: asdf';
+  public language = 'yaml';
+  public minimapEnabled = false;
+  public theme: 'vs-dark' = 'vs-dark';
+  public readonly = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private codeEditorService: CodeEditorService) {
+    this.codeEditorService.load();
   }
 
+  public Test() {
+    this.minimapEnabled = !this.minimapEnabled;
+  }
 }
--- stalker_ui/src/app/shared/widget/code-editor/code-editor.component.ts
+++ stalker_ui/src/app/shared/widget/code-editor/code-editor.component.ts
@@ -1,15 +1,123 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnDestroy, ViewChild } from '@angular/core';
+import { debounceTime, first, fromEvent, Subscription } from 'rxjs';
+import { CodeEditorService } from './code-editor.service';
 
+declare const monaco: any;
+
+// https://stackoverflow.com/questions/71072724/implement-monaco-editor-in-angular-13
+// https://github.com/atularen/ngx-monaco-editor
+// https://microsoft.github.io/monaco-editor/api/modules/monaco.editor.html
 @Component({
   selector: 'app-code-editor',
   templateUrl: './code-editor.component.html',
-  styleUrls: ['./code-editor.component.scss']
+  styleUrls: ['./code-editor.component.scss'],
 })
-export class CodeEditorComponent implements OnInit {
+export class CodeEditorComponent implements AfterViewInit, OnDestroy {
+  public _editor: any;
+  @ViewChild('editorContainer', { static: true }) _editorContainer!: ElementRef;
 
-  constructor() { }
+  private _code = '';
+  private _theme!: string;
+  private _minimapEnabled = true;
+  private _language!: string;
+  private _readonly = false;
+  private _tabSize = 2;
+  private _windowResizeSubscription: Subscription | undefined;
 
-  ngOnInit(): void {
+  private loadSub: Subscription | undefined;
+
+  @Input()
+  public set code(val: string) {
+    this._code = val;
+    if (this.codeEditorService.loaded && this._editor) {
+      this._editor.setValue(val);
+    }
   }
 
+  @Input()
+  public set language(val: string) {
+    this._language = val;
+    if (this.codeEditorService.loaded && this._editor) {
+      this._editor.setModelLanguage(val);
+    }
+  }
+
+  @Input()
+  public set minimapEnabled(val: boolean) {
+    this._minimapEnabled = val;
+    if (this.codeEditorService.loaded && this._editor) {
+      this._editor.updateOptions({ minimap: { enabled: val } });
+    }
+  }
+
+  @Input()
+  public set theme(val: 'vs' | 'vs-dark' | 'hc-black' | 'hc-light') {
+    this._theme = val;
+    if (this.codeEditorService.loaded && this._editor) {
+      this._editor._themeService.setTheme(val);
+    }
+  }
+
+  @Input()
+  public set readonly(val: boolean) {
+    this._readonly = val;
+    if (this.codeEditorService.loaded && this._editor) {
+      this._editor.updateOptions({ readOnly: val });
+    }
+  }
+
+  @Input()
+  public set tabSize(ts: number) {
+    this._tabSize = ts;
+    if (this.codeEditorService.loaded && this._editor) {
+      this._editor.getModel().updateOptions({ tabSize: ts });
+    }
+  }
+
+  constructor(private codeEditorService: CodeEditorService) {}
+
+  private initMonaco(): void {
+    if (!this.codeEditorService.loaded) {
+      this.loadSub = this.codeEditorService.loadingFinished.pipe(first()).subscribe(() => {
+        this.initMonaco();
+      });
+      return;
+    }
+
+    const options = {
+      value: this._code,
+      language: this._language,
+      minimap: {
+        enabled: this._minimapEnabled,
+      },
+      theme: this._theme,
+      readonly: this._readonly,
+      tabSize: this._tabSize,
+    };
+
+    this._editor = monaco.editor.create(this._editorContainer.nativeElement, options);
+
+    if (this._windowResizeSubscription) {
+      this._windowResizeSubscription.unsubscribe();
+    }
+    this._windowResizeSubscription = fromEvent(window, 'resize')
+      .pipe(debounceTime(0))
+      .subscribe(() => {
+        this._editor.layout();
+        console.log('resize :)');
+      });
+  }
+
+  ngAfterViewInit(): void {
+    this.initMonaco();
+  }
+
+  ngOnDestroy(): void {
+    if (this.loadSub) this.loadSub.unsubscribe();
+    if (this._windowResizeSubscription) this._windowResizeSubscription.unsubscribe();
+    if (this._editor) {
+      this._editor.dispose();
+      this._editor = undefined;
+    }
+  }
 }
