de4dfbf483e7641ae2d6f8ab39b6bac6db489799
Antoine Boisier-Michaud <antoine.boisier-michaud@cactusoft.ca>
2022-12-11 13:34:56 -0500

    Create test structure

--- flow_manager/src/modules/findings/findings.service.spec.ts
+++ flow_manager/src/modules/findings/findings.service.spec.ts
@@ -1,4 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../app.module';
+import { FindingsService } from './findings.service';
+
 describe('Findings Service Spec', () => {
+  let moduleFixture: TestingModule;
+  let findingsService: FindingsService;
+
+  beforeAll(async () => {
+    moduleFixture = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+    findingsService = moduleFixture.get(FindingsService);
+  });
+
+  beforeEach(async () => {});
+
   it('Test', () => {
     expect('lel').toEqual(2);
   });
