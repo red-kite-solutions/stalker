910b979d01b762e40ed8d8a03fc4b80124143147
Antoine Boisier-Michaud <antoine.boisier-michaud@cactusoft.ca>
2023-07-04 22:12:09 -0400

    Uniformize workflows

---
+++ stalker_ui/src/environments/environment.model.ts
@@ -0,0 +1,5 @@
+export interface Environment {
+  production: boolean;
+  fmUrl: string;
+  fmWsUrl: string;
+}
--- stalker_ui/src/environments/environment.prod.ts
+++ stalker_ui/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
-export const environment = {
+import { Environment } from './environment.model';
+
+export const environment: Environment = {
   production: true,
   fmUrl: 'http://localhost:3000', // change to proper fmUrl
+  fmWsUrl: 'http://localhost:3000', // change to proper fmWsUrl
 };
