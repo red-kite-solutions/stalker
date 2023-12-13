ff014f253998427ab11ace3de3c232357a04f888
lm-sec <sim.lacasse1239@gmail.com>
2023-12-13 00:09:16 -0500

    forgot to remove the code that was launching a hard coded job

--- flow_manager/src/modules/findings/commands/Findings/hostname.handler.ts
+++ flow_manager/src/modules/findings/commands/Findings/hostname.handler.ts
@@ -2,9 +2,7 @@ import { Logger } from '@nestjs/common';
 import { CommandHandler } from '@nestjs/cqrs';
 import { ConfigService } from '../../../database/admin/config/config.service';
 import { CustomJobsService } from '../../../database/custom-jobs/custom-jobs.service';
-import { JobFactory } from '../../../database/jobs/jobs.factory';
 import { JobsService } from '../../../database/jobs/jobs.service';
-import { DomainNameResolvingJob } from '../../../database/jobs/models/domain-name-resolving.model';
 import { EventSubscriptionsService } from '../../../database/subscriptions/event-subscriptions/event-subscriptions.service';
 import { SubscriptionTriggersService } from '../../../database/subscriptions/subscription-triggers/subscription-triggers.service';
 import { UserFindingHandlerBase } from '../user-findings-handler-base';
@@ -30,13 +28,5 @@ export class HostnameHandler extends UserFindingHandlerBase<HostnameCommand> {
     );
   }
 
-  protected async executeCore(command: HostnameCommand) {
-    // Launch a domain name resolving job
-    const job = JobFactory.createJob(DomainNameResolvingJob.name, [
-      { name: 'companyId', value: command.companyId },
-      { name: 'domainName', value: command.finding.domainName },
-    ]);
-
-    this.jobsService.publish(job);
-  }
+  protected async executeCore(command: HostnameCommand) {}
 }
