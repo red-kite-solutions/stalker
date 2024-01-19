23239be814c49d3659250d86d6bd471f45f08073
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2024-01-19 01:58:56 +0000

    remove fxlayout from edit companies

--- stalker_ui/src/app/shared/widget/spinner-button/spinner-button.component.ts
+++ stalker_ui/src/app/shared/widget/spinner-button/spinner-button.component.ts
@@ -2,8 +2,15 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-spinner-button',
-  templateUrl: './spinner-button.component.html',
-  styleUrls: ['./spinner-button.component.scss'],
+  template: `<button type="button" mat-flat-button [color]="buttonColor" [disabled]="loadingState">
+    <span class="tw-flex tw-gap-2 tw-items-center">
+      @if (loadingState) {
+        <mat-spinner [diameter]="16" [color]="spinnerColor"></mat-spinner>
+      }
+
+      <span>{{ label }}</span>
+    </span>
+  </button>`,
 })
 export class SpinnerButtonComponent {
   @Input() label = '';
