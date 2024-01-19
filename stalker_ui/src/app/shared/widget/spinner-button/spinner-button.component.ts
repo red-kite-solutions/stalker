03a5bf11bed96d460c102a49f6b366e5405197b6
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2024-01-19 02:52:18 +0000

    remove fx layout from companies list

--- stalker_ui/src/app/shared/widget/spinner-button/spinner-button.component.ts
+++ stalker_ui/src/app/shared/widget/spinner-button/spinner-button.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-spinner-button',
-  template: `<button type="button" mat-flat-button [color]="buttonColor" [disabled]="loadingState">
+  template: `<button class="tw-w-full" type="button" mat-flat-button [color]="buttonColor" [disabled]="loadingState">
     <span class="tw-flex tw-gap-2 tw-items-center">
       @if (loadingState) {
         <mat-spinner [diameter]="16" [color]="spinnerColor"></mat-spinner>
