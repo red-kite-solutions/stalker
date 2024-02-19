a27e1e320afb25a7a4791bfc5f3823bfc6a06ec9
lm-sec <sim.lacasse1239@gmail.com>
2024-02-19 09:38:54 -0500

    changing e2e tests to use keys and certs not on version control

--- flow_manager/src/modules/app.e2e-spec.ts
+++ flow_manager/src/modules/app.e2e-spec.ts
@@ -22,7 +22,7 @@ describe('AppController (e2e)', () => {
   it('Should get the root of the application to check if it is alive (GET /)', async () => {
     const r = await request(app.getHttpServer()).get('/');
     expect(r.statusCode).toBe(HttpStatus.OK);
-    expect(r.text).toBe('Hello World!');
+    expect(r.text.length).toBeGreaterThan(0);
   });
 
   it('Should be unauthorized to get the ping route while unauthenticated (GET /ping)', async () => {
