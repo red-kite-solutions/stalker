6bc83540e515f8b70ecc2dfbee29a299707b5e6f
lm-sec <sim.lacasse1239@gmail.com>
2024-02-27 15:55:04 -0500

    adding authorizations test

--- flow_manager/src/modules/database/reporting/port/port.e2e-spec.ts
+++ flow_manager/src/modules/database/reporting/port/port.e2e-spec.ts
@@ -70,59 +70,55 @@ describe('Port Controller (e2e)', () => {
       expect(r.statusCode).toBe(HttpStatus.OK);
       expect(r.body.length).toStrictEqual(0);
     });
+  });
 
-    it('Should have proper authorizations (GET /ports/:id)', async () => {
-      const success = await checkAuthorizations(
-        testData,
-        Role.ReadOnly,
-        async (givenToken) => {
-          return await getReq(
-            app,
-            givenToken,
-            `/ports/6450827d0ae00198f250672d`,
-          );
-        },
-      );
-      expect(success).toBe(true);
+  it('Should have proper authorizations (GET /ports/:id)', async () => {
+    const success = await checkAuthorizations(
+      testData,
+      Role.ReadOnly,
+      async (givenToken) => {
+        return await getReq(app, givenToken, `/ports/6450827d0ae00198f250672d`);
+      },
+    );
+    expect(success).toBe(true);
+  });
+
+  it('Should have proper authorizations (PUT /ports/:id/tags)', async () => {
+    const success = await checkAuthorizations(
+      testData,
+      Role.User,
+      async (givenToken) => {
+        return await putReq(
+          app,
+          givenToken,
+          `/ports/6450827d0ae00198f250672d/tags`,
+          {},
+        );
+      },
+    );
+    expect(success).toBe(true);
+  });
+
+  it('Should have proper authorizations (GET /ports)', async () => {
+    // Arrange
+    const project = await createProject(app, testData, getName());
+    const domain = 'www.example.org';
+    await createDomains(app, testData, project._id, [domain]);
+    const rHost = await postReq(app, testData.admin.token, `/hosts`, {
+      ips: ['192.168.2.1'],
+      projectId: project._id.toString(),
     });
 
-    it('Should have proper authorizations (PUT /ports/:id/tags)', async () => {
-      const success = await checkAuthorizations(
-        testData,
-        Role.User,
-        async (givenToken) => {
-          return await putReq(
-            app,
-            givenToken,
-            `/ports/6450827d0ae00198f250672d/tags`,
-            {},
-          );
-        },
-      );
-      expect(success).toBe(true);
-    });
+    const hostId = rHost.body[0]._id;
 
-    it('Should have proper authorizations (GET /ports)', async () => {
-      // Arrange
-      const project = await createProject(app, testData, getName());
-      const domain = 'www.example.org';
-      await createDomains(app, testData, project._id, [domain]);
-      const rHost = await postReq(app, testData.admin.token, `/hosts`, {
-        ips: ['192.168.2.1'],
-        projectId: project._id.toString(),
-      });
-
-      const hostId = rHost.body[0]._id;
-
-      const success = await checkAuthorizations(
-        testData,
-        Role.ReadOnly,
-        async (givenToken) => {
-          return await getReq(app, givenToken, `/ports?`);
-        },
-      );
-      expect(success).toBe(true);
-    });
+    const success = await checkAuthorizations(
+      testData,
+      Role.ReadOnly,
+      async (givenToken) => {
+        return await getReq(app, givenToken, `/ports?`);
+      },
+    );
+    expect(success).toBe(true);
   });
 
   it('Should have proper authorizations (DELETE /ports/:id)', async () => {
@@ -136,6 +132,17 @@ describe('Port Controller (e2e)', () => {
           `/ports/6450827d0ae00198f250672d`,
           {},
         );
+      },
+    );
+    expect(success).toBe(true);
+  });
+
+  it('Should have proper authorizations (DELETE /ports/)', async () => {
+    const success = await checkAuthorizations(
+      testData,
+      Role.User,
+      async (givenToken) => {
+        return await deleteReq(app, givenToken, `/ports/`, {});
       },
     );
     expect(success).toBe(true);
