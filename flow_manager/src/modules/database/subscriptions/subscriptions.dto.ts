89ad2e0f2cec8e8f74011870b9bf0f1d8c74e6fb
lm-sec <sim.lacasse1239@gmail.com>
2024-03-02 17:13:13 -0500

    more output from tests hopefully

--- flow_manager/src/modules/database/subscriptions/subscriptions.dto.ts
+++ flow_manager/src/modules/database/subscriptions/subscriptions.dto.ts
@@ -2,25 +2,27 @@ import { Type } from 'class-transformer';
 import { Equals, IsBoolean, IsIn, IsString } from 'class-validator';
 import { IsTypeIn } from '../../../validators/is-type-in.validator';
 
+const conditionOperators = [
+  'equals',
+  'gte',
+  'gt',
+  'lte',
+  'lt',
+  'contains',
+  'contains_i',
+  'startsWith',
+  'startsWith_i',
+  'endsWith',
+  'endsWith_i',
+  'equals_i',
+].flatMap((v) => [v, `not_${v}`]);
+
 export class JobConditionDto {
   @IsTypeIn(['string', 'number', 'boolean'])
   public lhs!: string | number | boolean;
 
   @IsString()
-  @IsIn([
-    'equals',
-    'gte',
-    'gt',
-    'lte',
-    'lt',
-    'contains',
-    'contains_i',
-    'startsWith',
-    'startsWith_i',
-    'endsWith',
-    'endsWith_i',
-    'equals_i',
-  ])
+  @IsIn(conditionOperators)
   public operator: string;
 
   @IsTypeIn(['string', 'number', 'boolean'])
