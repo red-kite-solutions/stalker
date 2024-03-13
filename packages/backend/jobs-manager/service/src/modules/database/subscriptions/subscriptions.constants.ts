47f792be4620e3c5aa126151709d6a6d864e39f4
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2024-03-13 03:04:28 +0000

    fix tests

--- packages/backend/jobs-manager/service/src/modules/database/subscriptions/subscriptions.constants.ts
+++ packages/backend/jobs-manager/service/src/modules/database/subscriptions/subscriptions.constants.ts
@@ -3,11 +3,11 @@ import { JM_ENVIRONMENTS } from '../../app.constants';
 // HACK: This should probably be configured with an environment variable,
 // but we kind of need to ship this workspaces feature.
 export const EVENT_SUBSCRIPTIONS_FILES_PATH =
-  process.env.JM_ENVIRONMENT === JM_ENVIRONMENTS.dev
-    ? './src/modules/database/subscriptions/cron-subscriptions/built-in/'
-    : '/server/dist/src/modules/database/subscriptions/event-subscriptions/built-in/';
+  process.env.JM_ENVIRONMENT === JM_ENVIRONMENTS.prod
+    ? '/server/dist/src/modules/database/subscriptions/event-subscriptions/built-in/'
+    : './src/modules/database/subscriptions/cron-subscriptions/built-in/';
 
 export const CRON_SUBSCRIPTIONS_FILES_PATH =
-  process.env.JM_ENVIRONMENT === JM_ENVIRONMENTS.dev
-    ? './src/modules/database/subscriptions/event-subscriptions/built-in/'
-    : '/server/dist/src/modules/database/subscriptions/cron-subscriptions/built-in/';
+  process.env.JM_ENVIRONMENT === JM_ENVIRONMENTS.prod
+    ? '/server/dist/src/modules/database/subscriptions/cron-subscriptions/built-in/'
+    : './src/modules/database/subscriptions/event-subscriptions/built-in/';
