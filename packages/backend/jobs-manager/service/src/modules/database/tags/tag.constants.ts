440e77daf55c5d3738680875652738fd09f950a2
lm-sec <sim.lacasse1239@gmail.com>
2024-06-14 21:57:20 -0400

    adding default tags

---
+++ packages/backend/jobs-manager/service/src/modules/database/tags/tag.constants.ts
@@ -0,0 +1,24 @@
+import { Tag } from './tag.model';
+
+export const DEFAULT_TAGS: Tag[] = [
+  {
+    color: '#610e16',
+    text: 'Critical',
+  },
+  {
+    color: '#be2a25',
+    text: 'High',
+  },
+  {
+    color: '#e79c06',
+    text: 'Medium',
+  },
+  {
+    color: '#f4ff00',
+    text: 'Low',
+  },
+  {
+    color: '#1043ff',
+    text: 'Info',
+  },
+];
