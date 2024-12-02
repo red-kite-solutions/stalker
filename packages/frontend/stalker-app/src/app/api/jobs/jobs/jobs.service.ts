bb96cb35d3f98454735e4c1c23839a01cb1a62c6
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2024-12-02 01:58:00 +0000

    fix merge

--- packages/frontend/stalker-app/src/app/api/jobs/jobs/jobs.service.ts
+++ packages/frontend/stalker-app/src/app/api/jobs/jobs/jobs.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { firstValueFrom, map, Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { CustomJob, CustomJobData } from '../../../shared/types/jobs/custom-job.type';
+import { Page } from '../../../shared/types/page.type';
+import { normalizeSearchString } from '../../../utils/normalize-search-string';
 
 @Injectable({
   providedIn: 'root',
@@ -10,8 +12,12 @@ import { CustomJob, CustomJobData } from '../../../shared/types/jobs/custom-job.
 export class JobsService {
   constructor(private http: HttpClient) {}
 
-  public getJobs(): Observable<CustomJob[]> {
-    return <Observable<CustomJob[]>>this.http.get(`${environment.fmUrl}/custom-jobs/`);
+  public getCustomJobs(filters: string[], page: number, pageSize: number): Observable<Page<CustomJob>> {
+    return this.http.get<CustomJob[]>(`${environment.fmUrl}/custom-jobs/`).pipe(
+      map((jobs) => jobs.sort((a, b) => a._id.localeCompare(b._id))),
+      map((jobs) => jobs.filter((job) => !filters?.length || this.filterJob(job, filters))),
+      map((jobs) => ({ items: jobs.slice(page * pageSize, page * pageSize + pageSize), totalRecords: jobs.length }))
+    );
   }
 
   public get(id: string): Observable<CustomJob> {
@@ -30,15 +36,20 @@ export class JobsService {
     return await firstValueFrom(this.http.put(`${environment.fmUrl}/custom-jobs/${id}`, data));
   }
 
-  public async duplicate(id: string) {
-    return await firstValueFrom(this.http.post(`${environment.fmUrl}/custom-jobs`, { jobId: id }));
-  }
-
   public async delete(id: string) {
     return await firstValueFrom(this.http.delete(`${environment.fmUrl}/custom-jobs/${id}`));
   }
 
+  public async duplicate(id: string) {
+    return await firstValueFrom(this.http.post(`${environment.fmUrl}/custom-jobs`, { jobId: id }));
+  }
+
   public async syncCache() {
     return await firstValueFrom(this.http.post(`${environment.fmUrl}/custom-jobs/sync`, undefined));
+  }
+
+  private filterJob(job: CustomJob, filters: string[]) {
+    const parts = [job.name, job.findingHandlerLanguage, job.type];
+    return filters.some((filter) => normalizeSearchString(parts.join(' ')).includes(filter));
   }
 }
