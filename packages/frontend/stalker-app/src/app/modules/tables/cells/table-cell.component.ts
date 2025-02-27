27b1c9943fc663daeed10f3eec85294f2f0880e3
Antoine Boisier-Michaud <aboisiermichaud@gmail.com>
2025-02-27 02:40:41 +0000

    Add more flexibility

--- packages/frontend/stalker-app/src/app/modules/tables/cells/table-cell.component.ts
+++ packages/frontend/stalker-app/src/app/modules/tables/cells/table-cell.component.ts
@@ -35,7 +35,7 @@ export class TableCellComponent {
     if (!this.tableField) return;
 
     const { findingKey, findingFieldKey } = this.tableField;
-    if (this.findingFields?.every((x) => x.data === 'True' || x.data === 'False')) {
+    if (this.findingFields?.every((x) => x.data?.toLowerCase() === 'True' || x.data?.toLowerCase() === 'False')) {
       if (findingFieldKey === 'mxRecords') return 'text';
       return 'boolean';
     }
